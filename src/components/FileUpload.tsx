27a19f97d7ac31f9cf52203c57012fe1722f6842
danishjamal <danishjaml326@gmail.com>
2023-09-23 23:35:56 +0530

    initial commit pinecone setup

--- src/components/FileUpload.tsx
+++ src/components/FileUpload.tsx
@@ -6,17 +6,19 @@ import React from "react";
 import { useDropzone } from "react-dropzone";
 import axios from "axios";
 import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
 
-const FileUpload = ({
-  file_key,
-  file_name,
-}: {
-  file_key: string;
-  file_name: string;
-}) => {
-  const [ uploading, setUploading ] = React.useState(false);
+const FileUpload = () => {
+  const router = useRouter();
+  const [uploading, setUploading] = React.useState(false);
   const { mutate, isLoading } = useMutation({
-    mutationFn: async () => {
+    mutationFn: async ({
+      file_key,
+      file_name,
+    }: {
+      file_key: string;
+      file_name: string;
+    }) => {
       const response = await axios.post("/api/create-chat", {
         file_key,
         file_name,
@@ -25,36 +27,36 @@ const FileUpload = ({
     },
   });
 
-  const { getInputProps, getRootProps } = useDropzone({
+  const { getRootProps, getInputProps } = useDropzone({
     accept: { "application/pdf": [".pdf"] },
     maxFiles: 1,
     onDrop: async (acceptedFiles) => {
-      console.log(acceptedFiles);
       const file = acceptedFiles[0];
       if (file.size > 10 * 1024 * 1024) {
-        toast.error("Please upload a smaller file");
-        //alert("Please upload a smaller file");
+        // bigger than 10mb!
+        toast.error("File too large");
         return;
       }
+
       try {
         setUploading(true);
         const data = await uploadToS3(file);
+        console.log("meow", data);
         if (!data?.file_key || !data.file_name) {
           toast.error("Something went wrong");
-          // alert("Something went wrong");
           return;
         }
         mutate(data, {
-          onSuccess: (data) => {
+          onSuccess: ({ chat_id }) => {
+            toast.success("Chat created!");
+            router.push(`/chat/${chat_id}`);
             console.log(data);
-           // toast.success(data.message);
           },
-          onError: (data) => {
-            toast.error("Error creating chats");
-            //console.log(data);
+          onError: (err) => {
+            toast.error("Error creating chat");
+            console.error(err);
           },
         });
-        //console.log("data: ", data);
       } catch (error) {
         console.log(error);
       } finally {
--- src/lib/pinecone.ts
+++ src/lib/pinecone.ts
@@ -1,4 +1,8 @@
-import { PineconeClient } from "@pinecone-database/pinecone";
+import {
+  PineconeClient,
+  Vector,
+  utils as PineconeUtils,
+} from "@pinecone-database/pinecone";
 import { downloadFromS3 } from "./s3-server";
 import { PDFLoader } from "langchain/document_loaders/fs/pdf";
 import { error } from "console";
@@ -6,6 +10,9 @@ import {
   Document,
   RecursiveCharacterTextSplitter,
 } from "@pinecone-database/doc-splitter";
+import { getEmbeddings } from "./db/embeddings";
+import md5 from "md5";
+import { convertToAsci } from "./utils";
 
 let pinecone: PineconeClient | null = null;
 export const getPineconeClient = async () => {
@@ -35,13 +42,46 @@ export async function loadS3IntoPinecone(filekey: string) {
   }
   const loader = new PDFLoader(file_name);
   const pages = (await loader.load()) as PDFPage[];
-  return pages;
+  //2. splitt and segment decuments
+  const documents = await Promise.all(pages.map(prepareDocument));
+
+  //3 embeded and vectorise individual documents
+  const vectors = await Promise.all(documents.flat().map(emebedeDocuments));
+
+  //4 uploade to PineconeDB
+
+  const client = await getPineconeClient();
+  const pinconeIndex = client.Index("chatpdf-dan");
+  console.log("Insertin vectors into Pinecone");
+  const namespace = convertToAsci(filekey);
+  PineconeUtils.chunkedUpsert(pinconeIndex, vectors, namespace, 10);
+
+  return documents[0];
 }
+
+export async function emebedeDocuments(doc: Document) {
+  try {
+    const embedding = await getEmbeddings(doc.pageContent);
+    const hash = md5(doc.pageContent);
+
+    return {
+      id: hash,
+      values: embedding,
+      metadata: {
+        text: doc.metadata.text,
+        pageNumber: doc.metadata.pageNumber,
+      },
+    } as Vector;
+  } catch (error) {
+    console.log("error embedding documents", error);
+    return error;
+  }
+}
+
 export const truncateStringByBytes = (str: string, bytes: number) => {
   const enc = new TextEncoder();
   return new TextDecoder("utf-8").decode(enc.encode(str).slice(0, bytes));
 };
-//2. splitt and segment decuments
 
 async function prepareDocument(page: PDFPage) {
   let { pageContent, metadata } = page;
